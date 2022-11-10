9b649ba7e2e4c7de19f8641e21ffc32b70803e1c
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2022-11-10 15:57:22 +0300

    Фильтрация массива

--- Program.cs
+++ Program.cs
@@ -5,3 +5,15 @@
     string  [] array=stroka.Split(" ");
     return array;
 }
+string [] array = CreateArray();
+int count = 0;
+for ( int i = 0; i < array.Lenght; i++)
+{
+    if ( array [i].Lenght <= 3)
+    {
+        array [count] = array [i];
+        count++;
+    }
+}
+
+
