9eceb03a17c67d7d82017f61b56c29177128388b
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2022-11-10 15:24:15 +0300

    Добавили метод для создания массива строк

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string [] CreateArray()
+{
+    Console.WriteLine("Введите массив строк через пробел");
+    string str = Console.ReadLine();
+    string  [] array=stroka.Split(" ");
+    return array;
+}
